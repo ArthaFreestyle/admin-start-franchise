bb9bc7a8fc77baf734e0def5ba0ecb3303bd833d
Artha Four <Arumifathina@gmail.com>
2026-05-14 16:28:51 +0700

    some updates

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-import { env } from 'process'
 
-const SUPABASE_URL = env.SUPABASE_URL
-const SUPABASE_KEY = env.SUPABASE_KEY
+// Langsung tembak ke process.env, jangan di-import dari 'process'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(SUPABASE_URL!, SUPABASE_KEY!)
+// Inisialisasi Supabase Client
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
